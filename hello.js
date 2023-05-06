3b0fee9e5d1a780bbc0185fb07777dc09fb4b913
Sunny Hundal <amandip.h@gmail.com>
2023-05-05 17:57:42 -0700

    create function to return an answer

--- hello.js
+++ hello.js
@@ -8,3 +8,9 @@ const sayHelloToConsole  = function (name) {
   console.log("Hello, " + name);
 }
 sayHelloToConsole('John');
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
