cc8b35ce35d77e5b1cb0d3e6aee7491b6f501431
Sunny Hundal <amandip.h@gmail.com>
2023-05-06 16:05:01 -0700

    create an array iterating over an array using a While loop

--- packing-list.js
+++ packing-list.js
@@ -4,3 +4,10 @@ console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+//iterating over an array using a While loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
